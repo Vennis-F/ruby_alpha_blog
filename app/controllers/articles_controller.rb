cc674e8af736cee566e4068b5b1ce9b466823e06
vennis <hoanganhgo28062001@gmail.com>
2024-09-11 15:57:48 +0700

    Add CRUD for resources articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,52 @@
 class ArticlesController < ApplicationController
-  def show
+  before_action :set_article, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @articles = Article.all
     # debugger
-    @article =  Article.find(params[":id"]) # Article.find(params["id"])
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit; end
+
+  def create
+    # debugger
+    @article = Article.new(article_params)
+    # render plain: article.inspect
+    if @article.save
+      flash[:notice] = "Article created successfully."
+      redirect_to @article # redirect_to article_path(article)
+    else
+      render "new"
+    end
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article updated successfully."
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article deleted successfully."
+    redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
